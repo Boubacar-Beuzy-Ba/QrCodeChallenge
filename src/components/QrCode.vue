71ed23b5949fb100ea2ac21a9569ae33ea14e5e7
Boubacar <bouba0178@gmail.com>
2022-09-10 01:34:09 +0000

    QR Code Challenge from FrontEnd Mentor Finished

---
+++ src/components/QrCode.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="container mx-auto max-w-sm rounded-2xl overflow-hidden shadow-lg flex justify-center items-center m-24 bg-white">
+    <div>
+        <div class="px-3 max-w-sm rounded-lg overflow-hidden shadow-lg">
+            <img class="w-full p-3 rounded-3xl" src="../../public/image-qr-code.png" alt="QrCode Image">
+        </div>
+        <div class="bg-white text-center px-12">
+            <h2 class="font-bold text-2xl">Improve your front-end skills by building Projects</h2>
+            <p class="text-gray-400 mb-7">Scan the QR code to visit Frontent Mentor and take your coding skills to the next level</p>
+        </div>
+    </div>
+</div>
+</template>
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script>
+  import QrCode from '../components/QrCode.vue';
+  export default {
+    components: {
+      QrCode,
+    },
+  }
+</script>
+<template>
+  <QrCode />
+</template>
